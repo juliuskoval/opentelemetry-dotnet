5da7844aec9bdbdf09d0b8328e87631dbef99e83
Julius Koval <juliuskoval@seznam.cz>
2026-03-12 21:36:35 +0100

    fix

--- src/Shared/TagWriter/TagWriter.cs
+++ src/Shared/TagWriter/TagWriter.cs
@@ -74,7 +74,7 @@ internal abstract class TagWriter<TTagState, TArrayState>
                     this.WriteStringTag(
                     ref state,
                     key,
-                    "TRUNCATED".AsSpan());
+                    Convert.ToString(value, CultureInfo.InvariantCulture).AsSpan());
                     return this.LogUnsupportedTagTypeAndReturnFalse(key, value);
                 }
 
